b77aa7c49869053ba56694304f8d31ec2decefbd
Asif950 <125110093+Asif950@users.noreply.github.com>
2023-03-11 14:24:59 +0600

    Update Asif.py

--- Asif.py
+++ Asif.py
@@ -1,7 +1,6 @@
 # Facebook: Bd RANBO. BY MAHDI
 import os,sys,time,json,random,re,string,platform,base64,uuid
 os.system("git pull")
-from bs4 import BeautifulSoup as sop
 from bs4 import BeautifulSoup
 import requests as ress
 ma4D1 = open
